b5ce3728fc193ce245798514b66bb59751691777
harveywxu <harveywxu@tencent.com>
2024-05-26 21:47:57 +0800

    encryption

--- encryption/aes_test.go
+++ encryption/aes_test.go
@@ -1,1 +1,45 @@
 package encryption
+
+import (
+	"bytes"
+	"crypto/aes"
+	"crypto/cipher"
+	"fmt"
+	"testing"
+)
+// AES 对称加密
+func TestAes(t *testing.T) {
+	// AES密钥，长度可以是16（AES-128）、24（AES-192）或32（AES-256）
+	key := []byte("myverystrongpasswordo32bitlength")
+
+	// 要加密的数据
+	plaintext := []byte("Hello, world!")
+
+	// 创建cipher.Block接口
+	block, err := aes.NewCipher(key)
+	if err != nil {
+		panic(err)
+	}
+
+	// 对数据进行填充，使其长度满足AES的要求
+	padding := aes.BlockSize - len(plaintext)%aes.BlockSize
+	padtext := bytes.Repeat([]byte{byte(padding)}, padding)
+	plaintext = append(plaintext, padtext...)
+
+	// 创建加密器
+	ciphertext := make([]byte, len(plaintext))
+	mode := cipher.NewCBCEncrypter(block, key[:aes.BlockSize])
+	mode.CryptBlocks(ciphertext, plaintext)
+
+	fmt.Printf("Ciphertext: %x\n", ciphertext)
+
+	// 创建解密器
+	mode = cipher.NewCBCDecrypter(block, key[:aes.BlockSize])
+	mode.CryptBlocks(ciphertext, ciphertext)
+
+	// 去掉填充
+	padding = int(ciphertext[len(ciphertext)-1])
+	plaintext = ciphertext[:len(ciphertext)-padding]
+
+	fmt.Printf("Plaintext: %s\n", plaintext)
+}
--- encryption/rsa_test.go
+++ encryption/rsa_test.go
@@ -1,1 +1,53 @@
 package encryption
+
+import (
+	"crypto"
+	"crypto/rand"
+	"crypto/rsa"
+	"crypto/sha256"
+	"fmt"
+	"testing"
+)
+
+// RSA 非对称加密
+func TestRsaSig(t *testing.T) {
+	// 生成一个 2048 位的 RSA 密钥对
+	privateKey, err := rsa.GenerateKey(rand.Reader, 2048)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	publicKey := &privateKey.PublicKey
+
+	// RSA 加密
+	plaintext := []byte("hello, world")
+	ciphertext, err := rsa.EncryptOAEP(sha256.New(), rand.Reader, publicKey, plaintext, nil)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	// RSA 解密
+	decryptedText, err := rsa.DecryptOAEP(sha256.New(), rand.Reader, privateKey, ciphertext, nil)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	fmt.Printf("Decrypted: %s\n", decryptedText)
+
+	// RSA 签名
+	hashed := sha256.Sum256(plaintext)
+	signature, err := rsa.SignPSS(rand.Reader, privateKey, crypto.SHA256, hashed[:], nil)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	// RSA 验证签名
+	err = rsa.VerifyPSS(publicKey, crypto.SHA256, hashed[:], signature, nil)
+	if err != nil {
+		fmt.Println("Could not verify signature: ", err)
+		return
+	}
+	fmt.Println("Signature verified")
+}
